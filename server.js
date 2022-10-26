b3c8678c4c0fe7d3597fe58a604c8c34fb19a2b3
NeilTomson <attilatoaso@yahoo.com>
2022-10-26 16:39:09 +0300

    start

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const mongoose =require("mongoose")
+const app  =express()
+const expresslayouts = require("express-ejs-layouts")
+
+
+const indexRouter = require("./routes/index")
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+}); 
+
+
+
+
+app.set("view engine","ejs")
+app.set("views",__dirname+"/views")
+app.set("layout","layouts/layout")
+app.use(expresslayouts)
+app.use(express.static("public"))
+
+app.use("/",indexRouter)
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`app runing on port 3000`)
+})
